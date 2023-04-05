c770683051dde8498b6d407c71735a6b098d2445
Tâm Phan <phantam04012003@gmail.com>
2023-04-05 21:18:32 +0700

    Add project files.

---
+++ Calendar/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.lb = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(27, 392);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(243, 392);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xoá";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(334, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(920, 369);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "End time";
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpEnd.Location = new System.Drawing.Point(27, 282);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(200, 22);
+            this.dtpEnd.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Start time";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CalendarTrailingForeColor = System.Drawing.Color.Green;
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpStart.Location = new System.Drawing.Point(27, 179);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(200, 22);
+            this.dtpStart.TabIndex = 8;
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(24, 54);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(44, 16);
+            this.lb.TabIndex = 7;
+            this.lb.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(27, 91);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(263, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Calendar/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        public void LoadData()
+        {
+            TamDB db = new TamDB();
+            dataGridView1.DataSource = db.Liches.ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var start = dtpStart.Value;
+            var end = dtpEnd.Value;
+
+            Lich appoint = new Lich();
+            appoint.Name = txtName.Text;
+            appoint.StartTime = start;
+            appoint.EndTime = end;
+
+           
+            TamDB db = new TamDB();
+            var list = db.Liches.ToList();
+            bool check = true;
+            Lich dateTrung = new Lich();
+            foreach(var d in list)
+            {
+                //int tmp = (start.Hour*60 + start.Minute) -( d.EndTime.Hour*60+d.EndTime.Minute);
+                if (DateTime.Compare(start, d.EndTime)<0)
+                {
+                    dateTrung = d;
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+            {
+                
+                db.Liches.Add(appoint);
+                db.SaveChanges();
+                MessageBox.Show("Thêm thành công");
+            }
+            else
+            {
+                DialogResult res = MessageBox.Show("Lịch trùng, bạn có muốn thay thế?", "Xác nhận", MessageBoxButtons.OKCancel);
+                if (res == DialogResult.OK)
+                {
+                    db.Liches.Remove(dateTrung);
+                    db.Liches.Add(appoint);
+                    db.SaveChanges();
+                    MessageBox.Show("Thêm thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Chỉnh lại thời gian");
+                }
+            }
+            LoadData();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count > 0)
+            {
+                int id = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+                TamDB db = new TamDB();
+                var appoint = db.Liches.Find(id);   
+                db.Liches.Remove(appoint);
+
+                db.SaveChanges();
+
+                LoadData();
+            }
+        }
+
+       
+    }
+}
